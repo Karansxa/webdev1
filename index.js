f2d6b0f530e855da2805df9cc893a441cffe8c1a
Karansxa <51290417+Karansxa@users.noreply.github.com>
2024-08-17 15:52:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let firstName = "Karan";
+let age = 21;
+var isStudetic = false;
+
+console.log(`Name: ${firstName}`);
+
+if (age >= 18) {
+    console.log(`Age: ${age} years old. You are eligible to vote.`);
+} else {
+    console.log(`Age: ${age} years old. You are not eligible to vote yet.`);
+}
+
+if (isStudetic) {
+    console.log("You are a student.");
+} else {
+    console.log("You are not a student.");
+}
+
